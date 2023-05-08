e60e7526516aadd2d888cd7059a1f98303ed3bcd
Xsmojie105 <18270851629@163.com>
2023-05-08 16:26:47 +0800

    上传发消息

---
+++ send_msg.py
@@ -0,0 +1,52 @@
+import socket
+import requests
+import json
+
+
+
+
+def send_msg(resp_dict):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    ip = '127.0.0.1'
+    client.connect((ip, 5700))
+
+    msg_type = resp_dict['msg_type']  # 回复类型（群聊/私聊）
+    number = resp_dict['number']  # 回复账号（群号/好友号）
+    msg = resp_dict['msg']  # 要回复的消息
+
+    # 将字符中的特殊字符进行url编码
+    msg = msg.replace(" ", "%20")
+    msg = msg.replace("\n", "%0a")
+
+    if msg_type == 'group':
+        payload = "GET /send_group_msg?group_id=" + str(
+            number) + "&message=" + msg + " HTTP/1.1\r\nHost:" + ip + ":5700\r\nConnection: close\r\n\r\n"
+    elif msg_type == 'private':
+        payload = "GET /send_private_msg?user_id=" + str(
+            number) + "&message=" + msg + " HTTP/1.1\r\nHost:" + ip + ":5700\r\nConnection: close\r\n\r\n"
+    print("发送" + payload)
+    client.send(payload.encode("utf-8"))
+    client.close()
+    return 0
+
+
+# 获取群成员列表
+def get_group(id):
+    response = requests.post('http://127.0.0.1:5700/get_group_member_list?group_id='+str(id)).json()
+    for i in response['data']:
+        if(i['card']!=''):
+            print (i['card']+str(i['user_id']))
+        else:
+            print (i['nickname']+str(i['user_id']))
+
+
+
+# resp_dict={'msg_type':'private','number':QQ号,'msg':'你好'}
+
+resp_dict={'msg_type': 'group', 'number': 554200909, 'msg': 'ooo'}
+
+
+send_msg(resp_dict)
+
+
